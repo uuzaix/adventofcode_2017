db315959025449473f80b0c00956420013c4deec
Larisa Dolgova <larisa.dolgova@gmail.com>
2018-01-07 21:39:16 +0100

    Filter and sum neighbours separatly

--- day1/day1.ts
+++ day1/day1.ts
@@ -2,6 +2,7 @@ import * as fs from "fs";
 
 type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 type Input = Array<Digit>;
+type FilteredInput = Array<Digit>;
 type Result = number;
 
 
@@ -9,22 +10,29 @@ const input: Input = fs.readFileSync("day1/day1.input", { encoding: "utf8" })
   .split('')
   .map((digit: string): Digit => parseInt(digit) as Digit);
 
-function sumNeighbours(input: Input, neighbourAt: number): Result {
-  return input
-    .reduce((acc: number, digit: number, index: number): number => {
-      if (digit === input[index + neighbourAt]
-        || (index >= neighbourAt && digit === input[index - neighbourAt])) {
-          return acc + digit
-        }
-        return acc
-  }, 0)
+function filterNeighbours(input: Input): FilteredInput {
+  return input.filter((digit: Digit, index: number): boolean => 
+    digit === input[index + 1] || (index + 1 === input.length && digit === input[0])
+  )
 }
 
-console.log(sumNeighbours([1, 2, 1, 3, 1, 4, 1, 5], 4), 'expect 4')
-console.log(sumNeighbours([1, 2, 3, 1, 2, 3], 3), 'expect 12')
-console.log(sumNeighbours([1, 2, 3, 4, 2, 5], 3), 'expect 4')
-console.log(sumNeighbours([1, 2, 2, 1], 2), 'expect 0')
-console.log(sumNeighbours([1, 2, 1, 2], 2), 'expect 6')
+function filterHalfwayNeighbours(input: Input, neighbourAt: number): FilteredInput {
+  return input.filter((digit: Digit, index: number): boolean =>
+    digit === input[index + neighbourAt]
+    || (index >= neighbourAt && digit === input[index - neighbourAt])
+  )
+}
 
-console.log('part 1: ', sumNeighbours(input, 1))
-console.log('part 2: ', sumNeighbours(input, input.length / 2))
+function sumFilteredNeighbours(input: FilteredInput): Result {
+  return input.reduce(((acc: number, digit: Digit): number => acc + digit), 0)
+}
+
+console.log(sumFilteredNeighbours(filterHalfwayNeighbours([1, 2, 1, 3, 1, 4, 1, 5], 4)), 'expect 4')
+console.log(sumFilteredNeighbours(filterHalfwayNeighbours([1, 2, 3, 1, 2, 3], 3)), 'expect 12')
+console.log(sumFilteredNeighbours(filterHalfwayNeighbours([1, 2, 3, 4, 2, 5], 3)), 'expect 4')
+console.log(sumFilteredNeighbours(filterHalfwayNeighbours([1, 2, 2, 1], 2)), 'expect 0')
+console.log(sumFilteredNeighbours(filterHalfwayNeighbours([1, 2, 1, 2], 2)), 'expect 6')
+console.log(sumFilteredNeighbours(filterNeighbours([1, 1, 2, 2])), 'expect 3')
+
+console.log('part 1: ', sumFilteredNeighbours(filterNeighbours(input)))
+console.log('part 2: ', sumFilteredNeighbours(filterHalfwayNeighbours(input, input.length / 2)))
